4eb0fa55494212ee669ee372aa3b81f1a7431a6a
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-06 16:50:55 +0300

    finalc commit

---
+++ project 1/adding more rows in table.sql
@@ -0,0 +1,10 @@
+INSERT INTO employees
+VALUES (2,"Squil","tent",15.50,"2023-01-03","071234545","squil@gmail.com"),
+(3,"John","Kemn",35.50,"2023-01-04","0723456778","john@gmail.com"),
+(4,"James","Kip",45.50,"2023-01-05","0721345678","james@gmail.com"),
+(5,"Joy","Che",65.50,"2023-01-06","0721345676","joy@gmail.com"),
+(6,"Nik","bet",15.50,"2023-01-07","0723456787","nik@gmail.com");
+
+
+
+SELECT * FROM employees
